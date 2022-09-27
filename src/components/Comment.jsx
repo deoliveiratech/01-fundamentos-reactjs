5dc6f911606b6d52f7b6a6fbba9d29361f0e7680
Rodrigo R. de Oliveira <digoliveira.tec@gmail.com>
2022-09-27 11:08:05 -0300

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src='https://github.com/digoliveiradev.png' />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rodrigo Rodrigues</strong>
+                            <time title='26 de Setembro às 20:29' dateTime="2022-09-26 20:29:20">Cerca de 1h atrás.</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
